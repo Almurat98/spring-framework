89f8c20d3eea652770570cafe7808be014d8ee00
Almurat98 <almurat908@gmail.com>
2023-02-22 12:08:51 -0600

    created class

--- Spring-01-Core-IOC/src/main/java/com/cydeo/bean_annotation/FullTimeMentor.java
+++ Spring-01-Core-IOC/src/main/java/com/cydeo/bean_annotation/FullTimeMentor.java
@@ -3,6 +3,6 @@ package com.cydeo.bean_annotation;
 public class FullTimeMentor {
 
     public void createAccount(){
-        System.out.println("FUll time mentor is created");
+        System.out.println("FUll time mentor is created!");
     }
 }
--- Spring-01-Core-IOC/src/main/java/com/cydeo/bean_annotation/PartTimeMentor.java
+++ Spring-01-Core-IOC/src/main/java/com/cydeo/bean_annotation/PartTimeMentor.java
@@ -3,6 +3,6 @@ package com.cydeo.bean_annotation;
 public class PartTimeMentor {
 
     public void createAccount(){
-        System.out.println("Part time mentor is created");
+        System.out.println("Part time mentor is created!");
     }
 }
