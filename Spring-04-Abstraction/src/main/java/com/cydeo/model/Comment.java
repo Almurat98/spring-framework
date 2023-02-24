ec1e7ef8572347b3656ba1028ada703ec1759ca7
Almurat98 <almurat908@gmail.com>
2023-02-23 18:28:52 -0600

    abstraction intro

---
+++ Spring-04-Abstraction/src/main/java/com/cydeo/model/Comment.java
@@ -0,0 +1,20 @@
+package com.cydeo.model;
+
+import lombok.Data;
+
+@Data
+public class Comment {
+        private String author;
+        private String text;
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Spring-04-Abstraction/src/main/java/com/cydeo/repository/DBCommentRepo.java
@@ -0,0 +1,10 @@
+package com.cydeo.repository;
+
+import com.cydeo.model.Comment;
+
+public class DBCommentRepo implements CommentRepo{
+    @Override
+    public void storeComment(Comment comment) {
+
+    }
+}
