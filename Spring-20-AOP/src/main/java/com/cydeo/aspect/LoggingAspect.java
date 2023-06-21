222a236804eedd8db6d80af65070d658088d2a4b
Almurat98 <almurat908@gmail.com>
2023-06-20 19:08:40 -0500

    logger 2.

---
+++ Spring-20-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -0,0 +1,30 @@
+package com.cydeo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class LoggingAspect {
+
+    Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+
+    @Pointcut("execution(* com.cydeo.controller.CourseController.*(..))")
+    private void pointcut(){}
+
+    @Before("pointcut()")
+    public void log(){
+        logger.info("Logger info ------");
+    }
+
+
+
+
+}
+
+
